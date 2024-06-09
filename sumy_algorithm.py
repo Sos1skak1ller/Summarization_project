83f1959378c983ad00fcdd822a799577b9a0452f
unknown <ivan.d.pokrovsky@gmail.com>
2024-06-09 20:59:32 +0300

    токенайзер переписан под более удобные библиотеки

--- sumy_algorithm.py
+++ sumy_algorithm.py
@@ -1,4 +1,3 @@
-# sumy_algorithm.py
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.nlp.stemmers import Stemmer
@@ -9,9 +8,13 @@ from tokenizer import tokenize_sentences, calculate_sentence_importance, extract
 
 LANGUAGE = "russian"
 
-def luhn_summarizer(text, num_sentences):
+def luhn_summarizer(text, num_sentences, word_weights_path=None):
     sentences = tokenize_sentences(text)
-    word_idf = extract_keywords(text)
+    
+    if word_weights_path:
+        word_idf = load_word_weights(word_weights_path)
+    else:
+        word_idf = extract_keywords(text)
     
     sentence_scores = [(sentence, calculate_sentence_importance(sentence, word_idf)) for sentence in sentences]
     sentence_scores = sorted(sentence_scores, key=lambda x: x[1], reverse=True)
@@ -19,7 +22,7 @@ def luhn_summarizer(text, num_sentences):
     top_sentences = [sentence for sentence, score in sentence_scores[:num_sentences]]
     return top_sentences
 
-def summarize_text(text, num_sentences, summarizer_type):
+def summarize_text(text, num_sentences, summarizer_type, word_weights_path=None):
     try:
         if summarizer_type.lower() == "lsa":
             parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
@@ -29,7 +32,7 @@ def summarize_text(text, num_sentences, summarizer_type):
             summary = summarizer(parser.document, num_sentences)
             return summary
         elif summarizer_type.lower() == "luhn":
-            return luhn_summarizer(text, num_sentences)
+            return luhn_summarizer(text, num_sentences, word_weights_path)
         elif summarizer_type.lower() == "lexrank":
             parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
             summarizer = LexRankSummarizer()
--- tokenizer.py
+++ tokenizer.py
@@ -1,22 +1,27 @@
-# tokenizer.py
 import re
 import math
 from collections import Counter
-from razdel import sentenize
-from pymorphy2 import MorphAnalyzer
+import nltk
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 
-morph = MorphAnalyzer()
+# Загрузка стоп-слов и инициализация стеммера для русского языка
+nltk.download('punkt')
+nltk.download('stopwords')
+stemmer = SnowballStemmer("russian")
+stop_words = set(stopwords.words("russian"))
 
 def tokenize_sentences(text):
-    sentences = [sentence.text for sentence in sentenize(text)]
-    return sentences 
+    sentences = sent_tokenize(text)
+    return sentences
 
 def lemmatize(word):
-    return morph.parse(word)[0].normal_form
+    return stemmer.stem(word)
 
 def calculate_sentence_importance(sentence, word_idf):
-    words = re.findall(r'\b\w+\b', sentence.lower())
-    words = [lemmatize(word) for word in words]
+    words = word_tokenize(sentence.lower())
+    words = [lemmatize(word) for word in words if word.isalnum() and word not in stop_words]
     sentence_length = len(words)
     if sentence_length == 0:
         return 0
@@ -24,8 +29,8 @@ def calculate_sentence_importance(sentence, word_idf):
     return sentence_importance
 
 def extract_keywords(text):
-    words = re.findall(r'\b\w+\b', text.lower())
-    words = [lemmatize(word) for word in words]
+    words = word_tokenize(text.lower())
+    words = [lemmatize(word) for word in words if word.isalnum() and word not in stop_words]
     word_frequencies = Counter(words)
     total_documents = len(tokenize_sentences(text))
     word_idf = {word: math.log10(total_documents / (1 + word_frequencies[word])) for word in word_frequencies}
